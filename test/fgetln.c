84d73535e68366c26e46d05d91ef4ee29439fa3b
John-Mark Bell <jmb@pexip.com>
2018-07-05 18:09:27 +0000

    Imported Upstream version 0.8.3

--- test/fgetln.c
+++ test/fgetln.c
@@ -24,14 +24,14 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/wait.h>
 #include <assert.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 #include <wchar.h>
+
+#include "test-stream.h"
 
 #define skip(msg) \
 	do { \
@@ -67,58 +67,6 @@ struct file {
 	int got_len;
 };
 
-static FILE *
-pipe_feed(const char *fmt, const void **buf, int buf_nmemb)
-{
-	FILE *fp;
-	int rc;
-	int pipefd[2];
-	pid_t pid;
-
-	rc = pipe(pipefd);
-	assert(rc >= 0);
-
-	pid = fork();
-	assert(pid >= 0);
-
-	if (pid == 0) {
-		int line;
-
-		/* Child writes data to pipe. */
-		rc = close(pipefd[0]);
-		assert(rc >= 0);
-
-		fp = fdopen(pipefd[1], "w");
-		assert(fp);
-
-		for (line = 0; line < buf_nmemb; line++) {
-			rc = fprintf(fp, fmt, buf[line]);
-			assert(rc >= 0);
-		}
-
-		rc = fclose(fp);
-		assert(rc >= 0);
-
-		_exit(0);
-	} else {
-		/* Parent gets a FILE and reads from it. */
-		rc = close(pipefd[1]);
-		assert(rc >= 0);
-
-		fp = fdopen(pipefd[0], "r");
-		assert(fp);
-
-		return fp;
-	}
-}
-
-static void
-pipe_close(FILE *fp)
-{
-	fclose(fp);
-	wait(NULL);
-}
-
 static void
 test_fgetln_single(void)
 {
@@ -149,7 +97,7 @@ test_fgetln_multi(void)
 		str = strdup("A\n");
 		str[0] += i;
 
-		files[i].lines = malloc(sizeof(char *) * LINE_COUNT);
+		files[i].lines = reallocarray(NULL, LINE_COUNT, sizeof(char *));
 		files[i].lines[0] = str;
 		files[i].lines[1] = str;
 		files[i].fp = pipe_feed("%s", files[i].lines, LINE_COUNT);
@@ -211,7 +159,7 @@ test_fgetwln_multi(void)
 		wstr = wcsdup(L"A\n");
 		wstr[0] += i;
 
-		files[i].lines = malloc(sizeof(char *) * LINE_COUNT);
+		files[i].lines = reallocarray(NULL, LINE_COUNT, sizeof(char *));
 		files[i].lines[0] = wstr;
 		files[i].lines[1] = wstr;
 		files[i].fp = pipe_feed("%ls", files[i].lines, LINE_COUNT);
